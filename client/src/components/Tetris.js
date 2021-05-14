ebcf54090caa1c8e01c91b5d11896e7a3b9d65df
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-05-14 09:37:51 -0700

    check for collissions

--- client/src/components/Tetris.js
+++ client/src/components/Tetris.js
@@ -1,28 +1,67 @@
-import React from "react";
+import { useState } from "react";
 // Components
 import Display from "./Display";
 import Stage from "./Stage";
 import StartButton from "./StartButton";
 // Functional Components
-import { createStage } from "../gameHelpers";
+import { createStage } from '../gameHelpers';
 // Styled Components
-import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
-
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
+// Custom Hooks
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
 
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+  // These two below are coming from our returns in the custom hooks we made
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [stage, setStage] = useStage(player);
 
+  console.log("re-render");
+
+  const movePlayer = (dir) => {
+    updatePlayerPos({x:dir,y:0})
+  }
+  const startGame = () => {
+    // Reset everything
+    setStage(createStage());
+    resetPlayer();
+  }
+  const drop = () => {
+    updatePlayerPos({x:0, y:1, collided: false})
+  }
+  const dropPlayer = () => {
+    drop();
+  }
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      // This is the left arrow
+      if (keyCode === 37) {
+        // move to the left
+        movePlayer(-1);
+      } else if (keyCode === 39){
+        movePlayer(1);
+      } else if (keyCode === 40) {
+        dropPlayer();
+      }
+    }
+  }
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role='button' tabIndex='0' onKeyDown={e => move(e)}>
       <StyledTetris>
-        <Stage stage={createStage()} />
+        <Stage stage={stage} />
         <aside>
-          <div>
-            
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
-          </div>
-          <StartButton />
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over" />
+          ) : (
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+          )}
+          <StartButton callback={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
--- client/src/gameHelpers.js
+++ client/src/gameHelpers.js
@@ -2,10 +2,10 @@ export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
 // FIRST Let's create our grid "Stage"
-export const createStage = () =>
-  // Creating a new array with a height of 20 rows
+// Creating a new array with a height of 20 rows
+// For each row, create an array with a width of 12
+// 0 and clear tell us that there is no tetromino in there atm
+  export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
-  // For each row, create an array with a width of 12
-    // 0 and clear tell us that there is no tetromino in there atm
-    new Array(STAGE_WIDTH).fill([0, 'clear'])
-  )
+    new Array(STAGE_WIDTH).fill([0, 'clear']),
+  );
